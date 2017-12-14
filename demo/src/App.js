741acfec043e8a24729d126e0bf94ca013d13dcb
marcusmolchany <marcusmolchany@gmail.com>
2017-12-14 09:37:25 -0800

    stuff

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,17 +1,32 @@
 import React, { Component } from 'react';
+
+import Jazzicon from 'react-jazzicon';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = {
+    seed: Array(40).fill(0),
+  };
+
+  onChange = ({ target: { value: seed } }) => {
+    this.setState(() => ({
+      seed
+    }));
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
+        <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
+          <h2>react-jazzicon</h2>
+        </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <Jazzicon diameter={100} seed={Math.random(100).toString()} />
+        </p>
+        <p>
+          <input type="text" onChange={this.onChange} placeholder="Type stuff here..." />
         </p>
       </div>
     );
