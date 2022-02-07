343293ad2b8736eac62e833adc5dc1f6ea83fafb
Marcus Molchany <marcusmolchany@gmail.com>
2022-02-07 10:10:27 -0800

    update storybook main config file

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,8 @@
 module.exports = {
   stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+    "../src/stories/**/*.stories.mdx",
+    "../src/stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials"],
   framework: "@storybook/react",
 };
-g;
